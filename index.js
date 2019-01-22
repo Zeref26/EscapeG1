cfdefedffb27cb1d2cda45765905b650c6f3c95f
Zeref26 <killianpoulain26@gmail.com>
2019-01-22 15:26:56 +0100

    Add files via upload

--- index.js
+++ index.js
@@ -5,6 +5,10 @@ bot.on('ready', () => {
     console.log('Bot is ready !');
 });
 
+bot.on("guildMemberAdd", member => {
+    member.addRole(bot.guilds.find('name',"Escape Game").roles.find('name',"Nouveau"));    
+});
+
 bot.on('message', (message) => {
     if (message.content.startsWith("-")) {
 
