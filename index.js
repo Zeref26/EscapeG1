bff84e89f97ff3877de285526626adbb0e11f126
Zeref26 <killianpoulain26@gmail.com>
2019-01-29 16:25:41 +0100

    Add files via upload

--- index.js
+++ index.js
@@ -6,13 +6,13 @@ bot.on('ready', () => {
 });
 
 bot.on("guildMemberAdd", member => {
-    member.addRole(bot.guilds.find('name',"Escape Game").roles.find('name',"Nouveau"));    
+    member.addRole(bot.guilds.find('name',"Escape Hub").roles.find('name',"Nouveau"));    
 });
 
 bot.on('message', (message) => {
-    let serv = bot.guilds.find('name',"Escape Game");
+    let serv = bot.guilds.find('name',"Escape Hub");
     let member = serv.members.find('id',message.author.id);
-    if (message.content.startsWith("-ready")) {
+    if (message.content == "-ready") {
         if (member.roles.exists('name',"Nouveau")) {
             member.send("Vouc êtes devenu un membre.");
             member.addRole(serv.roles.find('name',"Membre"));
@@ -21,6 +21,23 @@ bot.on('message', (message) => {
         } else {
             message.delete();
         }
+    }
+    if (message.content == "-partenariat") {
+        message.delete();
+        member.send({embed : {
+            color : "#0B1DA8",
+            author: {
+                name: client.user.username,
+                icon_url: client.user.avatarURL
+              },
+              title: "This is an embed",
+              url: "http://google.com",
+              description: "This is a test embed to showcase what they look like and what they can do.",
+              fields: [{
+                name: "Comment faire un partenariat ?",
+                value: "C'est très simple, il suffit d'envoyer votre fiche à un administrateur en ayant rempli les champs demandés.\nA la suite de celà, votre fiche sera publiée, ou non si il y a des problème avec celle-ci."
+              }]
+        }});
     }
 });
 
