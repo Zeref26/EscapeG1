82fd0da5d58a1b84b0f62b143256f10ee0e29bb5
Zeref26 <killianpoulain26@gmail.com>
2019-03-11 20:12:48 +0100

    Add files via upload

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ bot.on('message', message => {
     const args = message.content.slice(1).trim().split(/ +/g);
     const aut = message.guild.members.find('id',message.author.id);
     if (message.content.startsWith("-start")) {
-        message.guild.createChannel(aut.roles.find('hexColor',"#9033ca"),'category').then(channel => {
+        message.guild.createChannel(aut.roles.find('hexColor',"#9033ca").name,'category').then(channel => {
             channel.overwritePermissions(aut.roles.find('hexColor',"#9033ca"),{
                 SEND_MESSAGES: true,
                 READ_MESSAGES: true,
