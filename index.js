167a5a14d728354cd5959520aea2e9b8279faa70
Zeref26 <killianpoulain26@gmail.com>
2019-03-11 19:59:42 +0100

    Add files via upload

--- index.js
+++ index.js
@@ -7,6 +7,20 @@ bot.on('ready', () => {
 
 bot.on('message', message => {
     const args = message.content.slice(1).trim().split(/ +/g);
+    const aut = message.guild.members.find('id',message.author.id);
+    if (message.content.startsWith("-start")) {
+        message.guild.createChannel(aut.roles.find('hexColor',"#9033ca"),'category',[{
+            id: guild.id,
+            deny: ['SEND_MESSAGES','READ_MESSAGES','VIEW_CHANNEL'],
+            allow: []
+        }]).then(channel => {
+            channel.overwritePermissions(aut.roles.find('hexColor',"#9033ca"),{
+                SEND_MESSAGES: true,
+                READ_MESSAGES: true,
+                VIEW_CHANNEL: true
+            })
+        });
+    }
 });
 
 bot.login(process.env.TOKEN);
